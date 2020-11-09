6910529bdf3c3ce040f8d772631462aae46af323
maryan <genzhalo@gmail.com>
2020-11-09 19:50:10 +0200

    small fix

--- lib/route_defenition.dart
+++ lib/route_defenition.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'route_data.dart';
 
 abstract class RouteDefinition {
   String get segment;
   String get name;
-  List<Page> getPages(RouteEntry);
+  List<Page> getPages(RouteEntry data);
   List<RouteDefinition> get routes => [];
 }
